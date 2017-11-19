b3771d67ffd4f33f957ed4b454655b01957add83
Bhavya madhu <bhavyamadhu53@gmail.com>
2017-11-18 20:18:26 -0500

    component part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,6 +2,42 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
+const DISHES: Dish[] = [
+  {
+   name: 'uthappizza',
+   image: '/assets/images/uthappizza.png',
+   category: 'mains',
+   label: 'Hot',
+   price: '4.99',
+   description: 'A unique combination of Indian uthappam (pancake) and Italian pizza, topped with cerignola olives, ripe vine cherry tomatoes, vidalia onions, Guntur chillies and buffalo paneer. ',
+  },
+  {
+   name: 'zucchipakoda',
+   image: '/assets/images/zucchipakoda.png',
+   category: 'mains',
+   label: 'Hot',
+   price: '5.99',
+   description: 'Deep fried zucchini coated with mildly spiced chickpea flour batter accompanied with a sweet-tangy tamarind sauce', 
+   },
+   {
+   name: 'vadonut',
+   image: '/assets/images/vadonut.png',
+   category: 'appetizer', 
+   label: 'New',
+   price: '2.99',
+   description: 'A quintessential conFusion experience, is it a vada or it is a donut?'
+   },  
+   {
+   name: 'Elaicheese cake',
+   image: '/assets/images/elaicheesecake.png',
+   category: 'desert',
+   label: ' ',
+   price: '2.99',
+   description: 'A delectable, semi-sweet new york style cheese cake, with graham cracker crust and spiced with Indian cardamoms'
+ }
+];
+
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -9,41 +45,11 @@ import { Dish } from '../shared/dish';
 })
 export class MenuComponent implements OnInit {
 
-  dishes: Dish[] = [
-     {
-      name: 'uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      description: 'A unique combination of Indian uthappam (pancake) and Italian pizza, topped with cerignola olives, ripe vine cherry tomatoes, vidalia onions, Guntur chillies and buffalo paneer. ',
-     },
-     {
-      name: 'zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '5.99',
-      description: 'Deep fried zucchini coated with mildly spiced chickpea flour batter accompanied with a sweet-tangy tamarind sauce', 
-      },
-      {
-      name: 'vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer', 
-      label: 'New',
-      price: '2.99',
-      description: 'A quintessential conFusion experience, is it a vada or it is a donut?'
-      },  
-      {
-      name: 'Elaicheese cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'desert',
-      label: ' ',
-      price: '2.99',
-      description: 'A delectable, semi-sweet new york style cheese cake, with graham cracker crust and spiced with Indian cardamoms'
-    }
-  ];
+  dishes = DISHES;
+  
+  selectedDish = DISHES[0];
 
+  
   constructor() { }
 
   ngOnInit() {
