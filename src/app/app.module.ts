99c0262e6ee41e2325c9b84d5f2345e3223fb606
Bhavya <bhavyamadhu53@gmail.com>
2018-01-08 14:18:22 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -9,21 +11,39 @@ import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
     MaterialModule,
-    FlexLayoutModule
-
+    FlexLayoutModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ DishService, PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,41 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Dish } from '../shared/dish';
 
-const DISHES: Dish[] = [
-  {
-   name: 'uthappizza',
-   image: '/assets/images/uthappizza.png',
-   category: 'mains',
-   label: 'Hot',
-   price: '4.99',
-   description: 'A unique combination of Indian uthappam (pancake) and Italian pizza, topped with cerignola olives, ripe vine cherry tomatoes, vidalia onions, Guntur chillies and buffalo paneer. ',
-  },
-  {
-   name: 'zucchipakoda',
-   image: '/assets/images/zucchipakoda.png',
-   category: 'mains',
-   label: 'Hot',
-   price: '5.99',
-   description: 'Deep fried zucchini coated with mildly spiced chickpea flour batter accompanied with a sweet-tangy tamarind sauce', 
-   },
-   {
-   name: 'vadonut',
-   image: '/assets/images/vadonut.png',
-   category: 'appetizer', 
-   label: 'New',
-   price: '2.99',
-   description: 'A quintessential conFusion experience, is it a vada or it is a donut?'
-   },  
-   {
-   name: 'Elaicheese cake',
-   image: '/assets/images/elaicheesecake.png',
-   category: 'desert',
-   label: ' ',
-   price: '2.99',
-   description: 'A delectable, semi-sweet new york style cheese cake, with graham cracker crust and spiced with Indian cardamoms'
- }
-];
-
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -45,14 +11,18 @@ const DISHES: Dish[] = [
 })
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
-  
-  selectedDish = DISHES[0];
+    dishes: Dish[];
 
-  
-  constructor() { }
+    selectedDish: Dish;
+
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
+      this.dishes = this.dishService.getDishes();
+      }
+
+      onSelect(dish: Dish) {
+          this.selectedDish = dish;
   }
 
 }
